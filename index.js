fdc0224d7f97638817a4586252866ff90c3998d1
ronaldotj2002 <ronaldotj2002@yahoo.com.br>
2020-03-08 22:58:47 -0300

    Subindo arquivos

---
+++ index.js
@@ -0,0 +1,44 @@
+var http = require ('http');
+var express = require ('express');
+var app = express();
+var fs = require('fs');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.get("/chat", function(req, res){
+    console.log('HOMEPAGE - BRABO');
+    res.sendFile(__dirname + '/views/cliente.html')
+})
+
+app.use('/static', express.static('node_modules'));
+
+io.on('connection', function(socket){
+    console.log('Socket Cliente/Server');
+    
+    var news = [];
+    socket.emit('news', news);  
+   
+    socket.on('pontocheck', (data)=>{
+
+        news.push({'nome': data.nome, 'mensagem': data.mensagem});  
+        socket.emit('news', news);
+        
+        let nome = data.nome;
+        let mensagem = data.mensagem; 
+
+        fs.open('./papo.txt', 'a+', 0x777, (err, data)=>{
+            if(err) 
+            return console.error(err);      
+                 
+            fs.write(data, `${nome} ${mensagem}`, 'utf-8', (err)=>{
+                if(err)
+                return console.error(err);
+              
+            });
+        });        
+
+    });
+});
+
+server.listen(3010);
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Instalação das dependências
+    npm i
+
+# start Servidor
+    node index.js
+
+
+# fs (File System)
+
+    fs.open('./papo.txt', 'a+', 0x777,
+    abre o arquivo papo.txt, caso não encontre, a flag "a+" cria o
+    arquivo. 
+
+    Obs. O Arquivo 'papo.txt' precisa ter mermisão de escrita e leitura
+
